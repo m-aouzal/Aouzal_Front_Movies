7c10c9447eb18774ff89e16037db7ba11af32f2b
Siaouzal <aouzal1999@gmail.com>
2024-01-03 14:32:43 +0100

    deleted some unused files

--- src/app/guards/auth-can-activate-children.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivateChildFn, Router } from '@angular/router';
-import { inject } from '@angular/core';
-
-
-export const authCanActivateChildrenGuard: CanActivateChildFn = (childRoute, state) => {
- 
-  // return localStorage.getItem('role') === 'admin';
-  return true
-};
